e0646e589b64f9fd72872a452fbf85617f88a1ab
mamunawlad <133993137+mamunawlad@users.noreply.github.com>
2023-08-29 22:59:51 +0600

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,27 @@
+$(function () {
+
+
+$(`.hide`).click (function () {
+
+
+$(`.box`).slideUp(700)
+
+
+
+})
+
+
+
+$(`.show`).click(function () {
+
+
+
+    $(`.box`).slideDown(800)
+})
+
+$(`.toggle`).click(function () {
+
+    $(`.box`).slideToggle(700)
+})
+
+})
